9af922e65d01171f6bd27f25ff78c082186f1a41
devuser <devuser@yzmbp101.local>
2019-03-09 03:51:10 +0900

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+<<<<<<< HEAD
+            <h1 className="App-title">Fib Calculator version 2</h1>
+=======
+            <h1 className="App-title">Fib Calculater</h1>
+>>>>>>> origin/master
+            <Link to="/">Home</Link>
+            <Link to="/otherpage">Other Page</Link>
+          </header>
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/OtherPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+<<<<<<< HEAD
+      Im some other page!
+      <Link to="/">Go back home</Link>
+=======
+      Im some other page
+      <Link to="/">Go back to home page!</Link>
+>>>>>>> origin/master
+    </div>
+  );
+};
