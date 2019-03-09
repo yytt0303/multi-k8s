a86deab853e8e01a086302ca1dd6284f13006330
devuser <devuser@yzmbp101.local>
2019-03-09 16:34:05 +0900

    marge

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,13 +4,8 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <div>
-<<<<<<< HEAD
       Im some other page!
       <Link to="/">Go back home</Link>
-=======
-      Im some other page
-      <Link to="/">Go back to home page!</Link>
->>>>>>> origin/master
     </div>
   );
 };
